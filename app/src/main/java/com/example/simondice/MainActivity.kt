95860025cbbe1c2fe1d828a463f6c10b4ae91b34
DavidMacBook <davidmoralesluis@gmail.com>
2022-10-24 10:21:56 +0200

    add toDo

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.simondice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+
+    lateinit var simon: ImageView
+
+    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val empezar:Button = findViewById(R.id.startButton)
+        empezar.setOnClickListener {
+
+        }
+        //Todo: convertir imagen a .xml
+        simon = findViewById(R.drawable.simonStart)
+        simon.setImageResource(R.drawable.simonStart)
     }
 }
